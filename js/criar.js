3111085399e67e65e60866724d3c66049ee72db6
GiulianoBellati <giulianobellati@icloud.com>
2024-04-24 20:39:29 -0300

    Atualizando1

--- js/criar.js
+++ js/criar.js
@@ -1,11 +1,12 @@
-document.querySelector("#botao-cadastrar").addEventListener("click", () => {
+document.querySelector("#botao-criar").addEventListener("click", () => {
 
-    const form = document.querySelector("form")
+    const form = document.querySelector("ui form")
 
     const tarefa = {
         titulo: form.titulo.value,
-        descricao: form.descricao.value,
-        pontos: form.pontos.value
+        horaInicial: form.horaInicial.value,
+        horaFinal: form.horaFinal.value,
+        descricao: form.descricao.value
     }
 
     validar(tarefa)
@@ -27,18 +28,22 @@ function validar(tarefa){
         document.querySelector("#descricao-erro").innerText = "a descrição deve ter pelo menos 10 caracteres"
     }
     
-    if (tarefa.pontos <= 0){
-        document.querySelector("#pontos").classList.add("is-error")
-        document.querySelector("#pontos-erro").innerText = "pontos deve ser maior que zero"
+    if (tarefa.horaInicial.trim() == ""){
+        document.querySelector("#horaInicial").classList.add("is-error")
+        document.querySelector("#horaInicial-erro").innerText = "A hora inicial é obrigatória"
+    }
+
+    if (tarefa.horaFinal.trim() == ""){
+        document.querySelector("#horaFinal").classList.add("is-error")
+        document.querySelector("#horaFinal-erro").innerText = "A hora final é obrigatória"
     }
 }
 
 function limparErros(){
-    // TODO percorrer o array
+
     document
         .querySelectorAll("input .is-error, textarea .is-error")
         .classList
         .remove("is-error")
-
     
 }
